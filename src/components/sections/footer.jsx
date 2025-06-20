b085d44b64faf3253c35d3f490a43718b2a424fa
Pierre Beaufaron <p.beaufaron@gmail.com>
2025-06-20 17:39:35 +0200

    CSS

--- src/components/sections/footer.jsx
+++ src/components/sections/footer.jsx
@@ -7,22 +7,11 @@ const Footer = () => {
         <footer className="main-footer">
             <div className="footer-bottom pt-50 pb-40">
                 <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <div className="copyright-text">
-                                <p>
-                                    Copyright @{year}, <Link href="/">Pierre Beaufaron</Link> All
-                                    Rights Reserved.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="copyright-text extra-copyright">
-                                <p>
-                                    Crafted with theme_ocean
-                                </p>
-                            </div>
-                        </div>
+                    <div className="copyright-text">
+                        <p>
+                            Copyright @{year}, <Link href="/">Pierre Beaufaron</Link> All
+                            Rights Reserved.
+                        </p>
                     </div>
                 </div>
             </div>
